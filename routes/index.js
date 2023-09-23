49cb0a7ccf7963e6925f2522cdb9dbadc826dfe7
eric202209 <cher012410@gmail.com>
2023-09-23 10:43:55 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+/* File: index.js
+   Student’s Name: Hsin Tung, Chen
+   Student ID: 301270943
+   Date: September 22, 2023 */
+
+/* Code as below */
+
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+
+/* GET about me page. */
+router.get('/about', function (req, res, next) {
+  console.log('About page accessed');
+  res.render('about', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res, next) {
+  console.log('Projects page accessed');
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res, next) {
+  console.log('Services page accessed');
+  res.render('services', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res, next) {
+  console.log('Contact page accessed');
+  res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send-message', function (req, res, next) {
+  // Handle the form submission here (e.g., capture the form data and process it)
+  // Redirect back to the Home Page after processing the form data
+  res.redirect('/');
+});
+
+module.exports = router;
